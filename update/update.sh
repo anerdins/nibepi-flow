cb88c1222c0e000d0bce65ddedcaaa4ebc546f84
Fredrik Anerdin <60582895+anerdins@users.noreply.github.com>
2020-02-05 23:35:58 +0100

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -3,14 +3,15 @@ dirNode=$(find / -type f -name 'flows.json' 2>/dev/null | sed -r 's|/[^/]+$||' |
 if [[ ($dirNode != "") ]]
 then
 echo "Path found: ${dirNode}"
-echo "Stopping Node-RED..."
-sudo service nodered stop
+#echo "Stopping Node-RED..."
+#sudo service nodered stop
 mount=$(sudo mount -o remount,rw / 2>/tmp/tar_stderr);
 echo "Installing the NibePi addon to Node-RED"
 cd $dirNode && npm uninstall node-red-contrib-nibepi && npm install --save anerdins/node-red-contrib-nibepi#master
 echo "Downloading new flows for Node-RED"
 cd /tmp && wget https://raw.githubusercontent.com/anerdins/nibepi-flow/master/flows.json
 cd /tmp && mv -f flows.json $dirNode/flows.json
-echo "Starting Node-RED again."
-sudo service nodered start
+echo "Updated succesfully"
+echo "Restarting Node-RED."
+sudo service nodered restart
 fi
