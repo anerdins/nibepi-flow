51cf42ee7cf3513b22339c7b615610f9eff15c7e
Fredrik Anerdin <60582895+anerdins@users.noreply.github.com>
2020-02-24 00:11:13 +0100

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -7,7 +7,7 @@ echo "Stopping Node-RED..."
 sudo service nodered stop
 mount=$(sudo mount -o remount,rw / 2>/tmp/tar_stderr);
 echo "Installing the NibePi addon to Node-RED"
-cd $dirNode && npm uninstall node-red-contrib-nibepi && npm install --save anerdins/node-red-contrib-nibepi#snapshot
+cd $dirNode && npm uninstall node-red-contrib-nibepi && npm install --save anerdins/node-red-contrib-nibepi#master
 echo "Downloading new flows for Node-RED"
 mount=$(sudo mount -o remount,rw / 2>/tmp/tar_stderr);
 cd /tmp && wget https://raw.githubusercontent.com/anerdins/nibepi-flow/master/flows.json
