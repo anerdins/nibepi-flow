0742e1dc05bb06af41b6c61f7b202251cbe09c9c
Fredrik Anerdin <60582895+anerdins@users.noreply.github.com>
2020-05-09 22:12:20 +0200

    Update and rename update/bugfix/update-bugfixes.sh to update/update-bugfixes.sh

--- update/bugfix/update-bugfixes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for filename in bugfix-*.sh; do
-    [ -e "$filename" ] || continue
-    # ... rest of the loop body
-    echo "Running $filename"
-    sudo bash $filename
-    echo "Done with $filename"
-done
---
+++ update/update-bugfixes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo mount -o remount,rw /
+cd /tmp && \
+git clone -n https://github.com/anerdins/nibepi-flow.git --depth 1 && \
+cd nibepi-flow && \
+git checkout HEAD update/bugfix/update-bugfixes.sh && \
+mv update/bugfix /tmp && \
+rm -R -f /tmp/nibepi-flow
+
+for filename in /tmp/bugfix/bugfix-*.sh; do
+    [ -e "$filename" ] || continue
+    # ... rest of the loop body
+    echo "Running $filename"
+    sudo bash $filename
+    echo "Done with $filename"
+done
