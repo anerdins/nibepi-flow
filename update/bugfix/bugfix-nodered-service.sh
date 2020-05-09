4436ac3b59f7dcbdb1ed01f3c3390319e121e123
Fredrik Anerdin <60582895+anerdins@users.noreply.github.com>
2020-05-09 23:04:34 +0200

    Update bugfix-nodered-service.sh

--- update/bugfix/bugfix-nodered-service.sh
+++ update/bugfix/bugfix-nodered-service.sh
@@ -1,4 +1,3 @@
 
 sudo mount -o remount,rw / && sudo sed -i 's/NODE_OPTIONS=--max_old_space_size=512/NODE_OPTIONS=--max_old_space_size=256/g' /lib/systemd/system/nodered.service
 sudo systemctl daemon-reload
-sudo service nodered restart
