ba8edc8cc39dd55cdffd3a98348edf4724e05d28
Fredrik Anerdin <60582895+anerdins@users.noreply.github.com>
2022-09-09 13:07:54 +0200

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -1,17 +1,19 @@
-echo "Looking for Node-RED folder."
-dirNode=$(find / -type f -name 'flows.json' 2>/dev/null | sed -r 's|/[^/]+$||' |sort |uniq)
+echo "Setting up Node-RED folder."
+dirNode="/home/pi/.node-red"
 if [[ ($dirNode != "") ]]
 then
-echo "Path found: ${dirNode}"
+echo "Path: ${dirNode}"
 echo "Stopping Node-RED..."
 sudo service nodered stop
+echo "Setting RW mode on filesystem"
 mount=$(sudo mount -o remount,rw / 2>/tmp/tar_stderr);
 echo "Installing the NibePi addon to Node-RED"
-cd $dirNode && npm uninstall node-red-contrib-nibepi && npm install --save anerdins/node-red-contrib-nibepi#1.2
-echo "Downloading new flows for Node-RED"
-mount=$(sudo mount -o remount,rw / 2>/tmp/tar_stderr);
-cd /tmp && wget https://raw.githubusercontent.com/anerdins/nibepi-flow/1.2/flows.json && wget https://raw.githubusercontent.com/anerdins/nibepi-flow/1.2/package.json
-cd /tmp && mv -f flows.json $dirNode/flows.json && mv -f package.json $dirNode/package.json
+cd /tmp && git clone https://github.com/anerdins/nibepi-flow/ && cd nibepi-flow/ && git checkout 1.2 && cp -r flows.json /home/pi/.node-red/flows.json && cp -r package.json /home/pi/.node-red && cd .. && sudo rm -R nibepi-flow/
+
+cd /tmp && git clone https://github.com/anerdins/nibepi/ && cd nibepi/ && git checkout 1.2 && sudo rm -R /home/pi/.node-red/node_modules/nibepi/* && cp -r * /home/pi/.node-red/node_modules/nibepi/ && cd .. && sudo rm -R nibepi
+
+cd /tmp && git clone https://github.com/anerdins/node-red-contrib-nibepi.git && cd node-red-contrib-nibepi/ && git checkout 1.2 && sudo rm -R /home/pi/.node-red/node_modules/node-red-contrib-nibepi/* && cp -r * /home/pi/.node-red/node_modules/node-red-contrib-nibepi/ && cd .. && sudo rm -R node-red-contrib-nibepi/
+
 echo "Updated succesfully"
 echo "Starting Node-RED."
 sudo service nodered start
